1e56b76c3e1770b288e6eb64d9f222d9b8547a50
gogasan24ru <a@gogasan.tk>
2018-11-16 00:40:51 +0300

    some usefull comments, replace default port to allow run on agent-enabled hosts

--- example.php
+++ example.php
@@ -9,12 +9,12 @@ foreach (glob("src/*.php") as $filename)
 }
 
 
-$agent = ZabbixAgent::create(10051);
+$agent = ZabbixAgent::create(10351); //-p 10351 for zabbix_get
 
 $agent->start();
 
 $agent->setItem("some.key", ZabbixTimeDuration::now());
-$agent->setItem("agent.ping", ZabbixPrimitiveItem::create("1"));
+$agent->setItem("agent.ping", ZabbixPrimitiveItem::create("1")); //zabbix_get -s 127.0.0.1 -p 10351 -k "agent.ping"
 
 while (true) {
     echo "Usefull payload\n";
