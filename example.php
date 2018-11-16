87fc565af45b666d8274a881e9b2d5b859112bef
gogasan24ru <a@gogasan.tk>
2018-11-16 22:57:42 +0300

    modify example script for demo

--- example.php
+++ example.php
@@ -1,8 +1,5 @@
 <?php
-
 //include("vendor/autoload.php");
-
-
 foreach (glob("src/*.php") as $filename)
 {
     include $filename;
@@ -10,11 +7,32 @@ foreach (glob("src/*.php") as $filename)
 
 
 $agent = ZabbixAgent::create(10351); //-p 10351 for zabbix_get
+$agent->setDebugLevel();
+$agent -> setupActive("pi.local",
+    10051,
+    "PHP-zabbix-agent",
+    "agent_debug_dev");
 
 $agent->start();
 
+function get_server_memory_usage(){
+
+    $free = shell_exec('free');
+    $free = (string)trim($free);
+    $free_arr = explode("\n", $free);
+    $mem = explode(" ", $free_arr[1]);
+    $mem = array_filter($mem);
+    $mem = array_merge($mem);
+    $memory_usage = $mem[2]/$mem[1]*100;
+
+    return $memory_usage;
+}
+
 $agent->setItem("some.key", ZabbixTimeDuration::now());
 $agent->setItem("agent.ping", ZabbixPrimitiveItem::create("1")); //zabbix_get -s 127.0.0.1 -p 10351 -k "agent.ping"
+$agent->setItem("agent.hostname", ZabbixPrimitiveItem::create("PHP-zabbix-agent"));
+$agent->setItem("agent.version", ZabbixPrimitiveItem::create("PHP-zabbix-agent-0.0.1"));
+$agent->setItem("os.mem", ZabbixPrimitiveItem::create(trim(get_server_memory_usage(),'%')));
 
 while (true) {
     echo "Usefull payload\n";
@@ -23,3 +41,13 @@ while (true) {
 
     usleep(500000);
 }
+
+
+
+
+
+
+
+
+
+
