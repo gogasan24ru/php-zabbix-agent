f4838ed2a7248e48ab6ed44b159eb8b8fcf9d12f
gogasan24ru <a@gogasan.tk>
2018-11-23 15:46:43 +0300

    add active only support

--- src/ZabbixAgent.php
+++ src/ZabbixAgent.php
@@ -127,6 +127,12 @@ class ZabbixAgent
      */
     function __construct($host, $port)
     {
+        if(isset($port))
+        $this->setupPassive($host,$port);
+    }
+
+    public function setupPassive($host, $port)
+    {
         if (empty($host)) {
             throw new ZabbixAgentException("You must set host");
         }
@@ -138,6 +144,7 @@ class ZabbixAgent
         $this->port = $port;
         $this->host = $host;
     }
+
 
     public function setDebugLevel($level=PHPZA_LL_DEBUG)
     {
@@ -204,8 +211,11 @@ class ZabbixAgent
      */
     public static function create($port, $host = "0.0.0.0")
     {
-        return new ZabbixAgent($host, $port);
+        if(isset($port))
+            return new ZabbixAgent($host, $port);
+        return new ZabbixAgent();
     }
+
 
     /**
      * Start listen socket.
@@ -214,32 +224,34 @@ class ZabbixAgent
      */
     public function start()
     {
-        $this->listenSocket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-        if ($this->listenSocket === false) {
-            throw new ZabbixAgentSocketException('Create socket error.');
-        }
+        if(isset($port)) {
+            $this->listenSocket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+            if ($this->listenSocket === false) {
+                throw new ZabbixAgentSocketException('Create socket error.');
+            }
 
-        $setOptionResult = socket_set_option($this->listenSocket, SOL_SOCKET, SO_REUSEADDR, 1);
-        if ($setOptionResult === false) {
-            throw new ZabbixAgentSocketException('Set socket option error.');
-        }
+            $setOptionResult = socket_set_option($this->listenSocket, SOL_SOCKET, SO_REUSEADDR, 1);
+            if ($setOptionResult === false) {
+                throw new ZabbixAgentSocketException('Set socket option error.');
+            }
 
-        $bindResult = socket_bind($this->listenSocket, $this->host, $this->port);
-        if ($bindResult === false) {
-            throw new ZabbixAgentSocketException('Socket bind error.');
-        }
+            $bindResult = socket_bind($this->listenSocket, $this->host, $this->port);
+            if ($bindResult === false) {
+                throw new ZabbixAgentSocketException('Socket bind error.');
+            }
 
-        $listenResult = socket_listen($this->listenSocket, 0);
-        if ($listenResult === false) {
-            throw new ZabbixAgentSocketException('Socket listen error.');
-        }
+            $listenResult = socket_listen($this->listenSocket, 0);
+            if ($listenResult === false) {
+                throw new ZabbixAgentSocketException('Socket listen error.');
+            }
 
-        $nonBlockResult = socket_set_nonblock($this->listenSocket);
-        if ($nonBlockResult === false) {
-            throw new ZabbixAgentSocketException('Socket set nonblocking error.');
+            $nonBlockResult = socket_set_nonblock($this->listenSocket);
+            if ($nonBlockResult === false) {
+                throw new ZabbixAgentSocketException('Socket set nonblocking error.');
+            }
+            $this->logger(PHPZA_LL_INFO, __FUNCTION__ . " done");
+            return $this;
         }
-        $this->logger(PHPZA_LL_INFO,__FUNCTION__." done");
-        return $this;
     }
 
 
@@ -460,46 +472,47 @@ class ZabbixAgent
      */
     public function tick()
     {
-        //setup socket for incoming connections
-        try {
-            /**
-             * @todo fix @
-             */
-            $connection = @socket_accept($this->listenSocket);
-        } catch (Exception $e) {
-            /*
-             * Some implementations could transform php-errors to exceptions
-             */
-            throw new ZabbixAgentSocketException('Socket error on accept.');
-        }
+        if(isset($this->port)){
+            //setup socket for incoming connections
+            try {
+                /**
+                 * @todo fix @
+                 */
+                $connection = @socket_accept($this->listenSocket);
+            } catch (Exception $e) {
+                /*
+                 * Some implementations could transform php-errors to exceptions
+                 */
+                throw new ZabbixAgentSocketException('Socket error on accept.');
+            }
 
-        //commands processing
-        if ($connection > 0) {
-            $commandRaw = socket_read($connection, 1024);
+            //commands processing
+            if ($connection > 0) {
+                $commandRaw = socket_read($connection, 1024);
 
-            if ($commandRaw !== false) {
-                $command = trim($commandRaw);
-//                var_dump($commandRaw);
-//                echo "<hr>";
-//                var_dump($command);
-                try {
-                    $agentItem = $this->getItem($command);
-                    $buf = ZabbixProtocol::serialize($agentItem);
-                } catch (Exception $e) {
+                if ($commandRaw !== false) {
+                    $command = trim($commandRaw);
+    //                var_dump($commandRaw);
+    //                echo "<hr>";
+    //                var_dump($command);
+                    try {
+                        $agentItem = $this->getItem($command);
+                        $buf = ZabbixProtocol::serialize($agentItem);
+                    } catch (Exception $e) {
+                        socket_close($connection);
+                        throw new ZabbixAgentException("Serialize item error.", 0, $e);
+                    }
+
+                    $writeResult = socket_write($connection, $buf, strlen($buf));
                     socket_close($connection);
-                    throw new ZabbixAgentException("Serialize item error.", 0, $e);
+                    if ($writeResult === false) {
+                        throw new ZabbixAgentSocketException('Socket write error.');
+                    }
+                } else {
+                    throw new ZabbixAgentSocketException('Socket read error.');
                 }
-
-                $writeResult = socket_write($connection, $buf, strlen($buf));
-                socket_close($connection);
-                if ($writeResult === false) {
-                    throw new ZabbixAgentSocketException('Socket write error.');
-                }
-            } else {
-                throw new ZabbixAgentSocketException('Socket read error.');
             }
         }
-
         if($this->activeAvailable)
         {
             $this->checkForActiveChecksUpdates();
